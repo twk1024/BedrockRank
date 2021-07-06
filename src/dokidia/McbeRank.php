ac5015d1905efff4b2d10325cd191beb15404b4f
dokidia <42838268+twk1024@users.noreply.github.com>
2021-07-06 23:25:41 +0900

    Command Bug Fix
    - Fixed the bug occured when running the command

--- src/dokidia/McbeRank.php
+++ src/dokidia/McbeRank.php
@@ -43,15 +43,15 @@ class McbeRank extends PluginBase implements Listener{
 
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $array) : bool{
         if($cmd->getName() == "서버순위"){
-            $url = 'http://be.diamc.kr:3500/api/servers/diamc.kr:19132' . $this->db['domain'] . ':' . $this->db['port'];
+            $url = 'http://be.diamc.kr:3500/api/servers/' . $this->db['domain'] . ':' . $this->db['port'];
             $data = (array) json_decode(Internet::getURL($url));
             $rank = $data['rank'];
             $online = $data['online'];
             
             if($online == true){
-                $event->getPlayer()->sendMessage('§l§b[ §f알림 §b] §r§f현재 우리 서버의 순위는 §6' . $rank . '위§r§f 입니다! §r(MCBE RANK 기준)');
+                $sender->sendMessage('§l§b[ §f알림 §b] §r§f현재 우리 서버의 순위는 §6' . $rank . '위§r§f 입니다! §r(MCBE RANK 기준)');
             }else{
-                $event->getPlayer()->sendMessage('§l§b[ §f알림 §b] §r§fMCBE Rank에 이 서버가 등록되어 있지 않습니다. https://be.diamc.kr/new 에서 서버를 등록해 주세요.');
+                $sender->sendMessage('§l§b[ §f알림 §b] §r§fMCBE Rank에 이 서버가 등록되어 있지 않습니다. https://be.diamc.kr/new 에서 서버를 등록해 주세요.');
             }
             
         }
